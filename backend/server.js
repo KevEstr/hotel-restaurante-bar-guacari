a8ab4be819160fff922eb0d18f1ccb35f922aeed
Kev <kevin.estrada1@udea.edu.co>
2024-07-08 08:53:38 -0500

    Cambios CORS

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,7 @@ const corsOptions = {
         } else {
             callback(new Error('Not allowed by CORS'));
         }
-    },
-    credentials: true,
+    }
 };
 
 app.use(cors(corsOptions));
